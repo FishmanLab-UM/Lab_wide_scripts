934a8d119e6bd6ce71f9ed5dd39cf779e85effb1
Evansd36 <96156523+Evansd36@users.noreply.github.com>
2023-12-13 15:30:56 -0700

    Delete htseq_for_loop.sh
    Meant to put in its own folder

--- htseq_for_loop.sh
+++
@@ -1,1 +0,0 @@
-for i in *.split.bam; do htseq-count -f bam --nonunique=all --stranded=no --type=gene --idattr=Name --a 0 $i /scratch/evan/MimulusRPs_6/Allelic_ID_IM62_SF5_evan_made.gff3 > ${i:0:-43}.counts.tsv;done
